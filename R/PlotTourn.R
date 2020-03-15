d3936bc58b1f0727c87253ee4dabf66c4990a446
Alex Kaechele <xkaech@gmail.com>
2020-03-14 21:40:59 -0500

    Update PlotTourn.R

--- R/PlotTourn.R
+++ R/PlotTourn.R
@@ -12,28 +12,28 @@
 #'@return a ggplot object of a blank march madness tournament
 #'
 #'@export
-PlotTourn <- function(games=32){
-
+PlotTourn <- function(games=32, playin = NA){
+  
   # Taking into consideration a game occurs on a line
   games <- games/4
-
+  
   # Generate start and stop for line segment plots
-  lineCoord <- BracketLines(games = games)
-
+  lineCoord <- BracketLines(games = games, playin = playin)
+  
   # Locations for labels
   regions <- c("Region X", "Region W", "Region Y", "Region Z", "Championship")
   xregion <- c(2.5, 2.5, 6.5, 6.5, 4.5)
   yregion <- c(.5, 16.5, .5, 16.5, 17)
-
+  
   # Generating plots
   tournplot <- ggplot2::ggplot(data = lineCoord) +
     ggplot2::theme_void() +
     ggplot2::geom_segment(ggplot2::aes(x = x1, y = y1, xend = x2, yend = y2)) +
     ggplot2::annotate("text", x = xregion, y = yregion, label = regions)
-
+  
   # Return ggplot object
   return(tournplot)
-
+  
 }
 
 #' Determines X, Y Coordinates for MM Bracket
@@ -47,55 +47,88 @@ PlotTourn <- function(games=32){
 #'@return a data.frame of the beginning and end x and y coordinates to draw bracket
 #'
 #'@export
-BracketLines <- function(games){
-
+BracketLines <- function(games, playin){
+  
+  
+  
   # Initialize vectors
   x1 <- c()
   x2 <- c()
   y1 <- c()
   y2 <- c()
-
-
+  
+  
+  # Adding play in game
+  if(!min(is.na(playin))){
+    
+    # Loop though playin games
+    for(i in playin){
+      
+      # Extract for region
+      region <- substr(i, 1, 1)
+      
+      # Extract for seed (11, 16)
+      seed <- substr(i, 2, 3)
+      
+      if(region=="W"){
+        print("W")
+      } else if(region=="X"){
+        print("X")
+      } else if(region=="Z"){
+        print("Z")
+      } else if(region=="Y"){
+        print("Y")
+      }
+      # x1 <- append()
+      # y2 <- append()
+      # x2 <- append()
+      # y2 <- append()
+      # 
+    }
+    
+  }
+  
+  
   # Looping though the games to plot
   for(i in 0:(games-1)){
-
+    
     # Round 1
     x1 <- append(x1, c(0, 0, 1, 10, 10, 9))
     y1 <- append(y1, c(i*2+1, i*2+2, i*2+1, i*2+1, i*2+2, i*2+1))
     x2 <- append(x2, c(1, 1, 1, 9, 9, 9))
     y2 <- append(y2, c(i*2+1, i*2+2, i*2+2, i*2+1, i*2+2, i*2+2))
-
+    
     # Round 2
     if(i%%2){
-
+      
       x1 <- append(x1, c(1, 1, 2, 9, 9, 8))
       y1 <- append(y1, c(i*2-0.5, i*2+1.5, i*2-0.5, i*2-0.5, i*2+1.5, i*2-0.5))
       x2 <- append(x2, c(2, 2, 2, 8, 8, 8))
       y2 <- append(y2, c(i*2-0.5, i*2+1.5, i*2+1.5, i*2-0.5, i*2+1.5, i*2+1.5))
-
+      
     }
-
+    
     # Round 3
     if(i%%4==0){
-
+      
       x1 <- append(x1, c(2, 2, 3, 8, 8, 7))
       y1 <- append(y1, c(i*2+2.5, i*2+6.5, i*2+2.5, i*2+2.5, i*2+6.5, i*2+2.5))
       x2 <- append(x2, c(3, 3, 3, 7, 7, 7))
       y2 <- append(y2, c(i*2+2.5, i*2+6.5, i*2+6.5, i*2+2.5, i*2+6.5, i*2+6.5))
-
+      
     }
-
+    
     # Round 4
     if(i%%8 ==0){
-
+      
       x1 <- append(x1, c(3, 3, 4, 4, 5, 7, 7, 6))
       y1 <- append(y1, c(i*2+4.5, i*2+12.5, i*2+4.5, i*2+8.5, i*2+8.5, i*2+4.5, i*2+12.5, i*2+4.5))
       x2 <- append(x2, c(4, 4, 4, 5, 6, 6, 6, 6))
       y2 <- append(y2, c(i*2+4.5, i*2+12.5, i*2+12.5, i*2+8.5, i*2+8.5, i*2+4.5, i*2+12.5, i*2+12.5))
-
+      
     }
   }
-
+  
   return(data.frame(cbind(x1, x2, y1, y2)))
-
+  
 }
